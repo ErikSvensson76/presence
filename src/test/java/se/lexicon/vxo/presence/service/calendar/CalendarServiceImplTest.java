d3de79208a5179f7279b4e4792fe9bb8a0643819
Erik Svensson <eosvensson@gmail.com>
2020-05-15 09:10:18 +0200

    Added bean name for CalendarServiceImpl.java

--- src/test/java/se/lexicon/vxo/presence/service/calendar/CalendarServiceImplTest.java
+++ src/test/java/se/lexicon/vxo/presence/service/calendar/CalendarServiceImplTest.java
@@ -3,6 +3,7 @@ package se.lexicon.vxo.presence.service.calendar;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,6 +23,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class CalendarServiceImplTest {
 
     @Autowired
+    @Qualifier(CalendarService.JPA_IMPL_BEAN_NAME)
     private CalendarService calendarService;
 
     @Test
